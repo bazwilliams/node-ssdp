f3ce83fb5d702f1f055d79168a721462ab71005b
Barry John Williams <barry@bjw.me.uk>
2015-03-09 19:55:05 +0000

    Widened linn device search to openhome device search

--- examples/openhomeDevices.js
+++ examples/openhomeDevices.js
@@ -1,12 +1,12 @@
 var ssdp = require('../');
 
-const linnSources = 'urn:linn-co-uk:device:Source:1';
+const openhomeProducts = 'urn:av-openhome-org:service:Product:1';
 
 ssdp.on('DeviceFound', console.log);
 ssdp.on('DeviceAvailable:urn:av-openhome-org:service:Playlist:1', console.log);
 ssdp.on('DeviceUnavailable:urn:av-openhome-org:service:Playlist:1', console.log);
 ssdp.on('DeviceUpdate:urn:av-openhome-org:service:Playlist:1', console.log);
 
-ssdp.mSearch(linnSources);
+ssdp.mSearch(openhomeProducts);
 
 setTimeout(ssdp.close, 20000);
