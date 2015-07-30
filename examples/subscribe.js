929a2ee23aff0a00587abedb38f2b0f53e3f9e15
Barry John Williams <barry@bjw.me.uk>
2015-07-30 21:55:43 +0100

    Factored out non subscription code into subscribe example; added resubscribe and unsubscribe

--- examples/subscribe.js
+++ examples/subscribe.js
@@ -1,7 +1,45 @@
 var Subscription = require('../subscription.js');
+var responseParsers = require("../responseparsers.js");
+var _ = require('underscore');
+var xml2js = require('xml2js');
+var xmlParser = new xml2js.Parser({explicitArray: false});
+
+function elementText(element) {
+    return _.isObject(element) ? element._ : element;
+}
+function first(element) {
+    return _.isArray(element) ? element[0] : element;
+}
+
+function infoEvent(result, callback) {
+    if (result['e:propertyset']['e:property']) {
+      var dictionary = _.chain(result['e:propertyset']['e:property'])
+        .reduce(_.extend, {})
+        .pick('Uri', 'Metadata')
+        .value();
+      if (dictionary.Metadata) {
+        xmlParser.parseString(dictionary.Metadata, function (err, result) {
+          callback(null, {
+            uri: dictionary.Uri,
+            artist: elementText(first(result['DIDL-Lite'].item['upnp:artist'])),
+            title: elementText(first(result['DIDL-Lite'].item['dc:title'])),
+            albumArt: elementText(first(result['DIDL-Lite'].item['upnp:albumArtURI'])),
+            album: elementText(first(result['DIDL-Lite'].item['upnp:album']))
+          });
+        });
+      } else {
+        callback(new Error('No metadata found'));
+      }
+    } else {
+        callback(new Error('No track found'));
+    }
+}
 
 var host = '192.168.1.127';
 var port = 55178;
-var eventSub = '/Ds/Info/event';
+var infoSub = '/Ds/Info/event';
+var productSub = '/Ds/Product/event';
 
-var dsSub = new Subscription(host, port, eventSub, function(err, data) { console.log(data); });
+var infoSub = new Subscription(host, port, infoSub, responseParsers.xml(infoEvent, function(err, data) { console.log(data); }));
+
+setTimeout(infoSub.unsubscribe, 10000);
--- subscription.js
+++ subscription.js
@@ -1,47 +1,28 @@
 var http = require('http');
 var portfinder = require('portfinder');
 var ip      = require("ip");
-var responseParsers = require("./responseparsers.js");
-var _ = require('underscore');
-var xml2js = require('xml2js');
-var xmlParser = new xml2js.Parser({explicitArray: false});
 
-function elementText(element) {
-    return _.isObject(element) ? element._ : element;
-}
-function first(element) {
-    return _.isArray(element) ? element[0] : element;
-}
-
-function toTrack(result, callback) {
-    if (result['e:propertyset']['e:property']) {
-      var dictionary = _.chain(result['e:propertyset']['e:property'])
-        .reduce(_.extend, {})
-        .pick('Uri', 'Metadata')
-        .value();
-      if (dictionary.Metadata) {
-        xmlParser.parseString(dictionary.Metadata, function (err, result) {
-          callback(null, {
-            uri: dictionary.Uri,
-            artist: elementText(first(result['DIDL-Lite'].item['upnp:artist'])),
-            title: elementText(first(result['DIDL-Lite'].item['dc:title'])),
-            albumArt: elementText(first(result['DIDL-Lite'].item['upnp:albumArtURI'])),
-            album: elementText(first(result['DIDL-Lite'].item['upnp:album']))
-          });
-        });
-      } else {
-        callback(new Error('No metadata found'));
+function Subscription(host, port, eventSub, callback) {
+  var sid, resubscribeInterval, timeoutSeconds = 1800, httpSubscriptionResponseServer;
+  this.unsubscribe = function() {
+    clearInterval(resubscribeInterval);
+    httpSubscriptionResponseServer.close();
+    http.request({
+      host: host,
+      port: port,
+      path: eventSub,
+      method: 'UNSUBSCRIBE',
+      headers: {
+        'SID': sid
       }
-    } else {
-        callback(new Error('No track found'));
-    }
-}
+    }, function(res) {
+      console.log('Unsubscribe: '+ res.statusCode);
+    }).end();
+  };
 
-function Subscribe(host, port, eventSub, callback) {
   portfinder.getPort(function (err, availablePort) {
-    var httpSubscriptionResponseServer = http.createServer();
-    httpSubscriptionResponseServer.on('request', responseParsers.xml(toTrack, callback));
-
+    httpSubscriptionResponseServer = http.createServer();
+    httpSubscriptionResponseServer.on('request', callback);
     httpSubscriptionResponseServer.listen(availablePort, function() {
       http.request({
         host: host,
@@ -51,12 +32,28 @@ function Subscribe(host, port, eventSub, callback) {
         headers: {
           'CALLBACK': "<http://" + ip.address() + ':' + availablePort + ">",
           'NT': 'upnp:event',
-          'TIMEOUT': 'Second-30'
+          'TIMEOUT': 'Second-' + timeoutSeconds
         }
       }, function(res) {
-        // console.log(res.statusCode);
+        console.log('Subscribe: '+ res.statusCode + '; sid: ' + res.headers.sid);
+        sid = res.headers.sid;
       }).end();
     });
+
+    resubscribeInterval = setInterval(function() {
+      http.request({
+        host: host,
+        port: port,
+        path: eventSub,
+        method: 'SUBSCRIBE',
+        headers: {
+          'SID': sid,
+          'TIMEOUT': 'Second-' + timeoutSeconds
+        }
+      }, function(res) {
+        console.log('Resubscribe: '+ res.statusCode);
+      }).end();
+    }, (timeoutSeconds-1) * 1000)
   });
 }
-module.exports = Subscribe;
+module.exports = Subscription;
